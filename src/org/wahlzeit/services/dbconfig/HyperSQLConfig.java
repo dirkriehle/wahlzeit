ecfca4d7b4f9ddb1776b1d5bb7d25d903beb7b46
Flo <florian.weikert@gmx.de>
2012-10-26 21:22:50 +0200

    Added some more fixes concerning HSQL. However, HSQL does not like the SQL script :(
    Other problems: DatabaseConnection is ugly due to the excessive use of
    static methods -> Needs some refactoring

--- src/org/wahlzeit/services/dbconfig/HyperSQLConfig.java
+++ src/org/wahlzeit/services/dbconfig/HyperSQLConfig.java
@@ -1,57 +1,57 @@
 package org.wahlzeit.services.dbconfig;
 
 import org.hsqldb.server.Server;
-import org.wahlzeit.services.SysConfig;
 import org.wahlzeit.services.SysLog;
 
 public class HyperSQLConfig implements DataBaseConfig {
 	protected final String dbName = "wahlzeit";
-	
-	protected final String driver = "org.hsqldb.jdbcDriver";
-	protected final String connection = "jdbc:hsqldb:hsql://localhost/" + dbName;
 	protected Server server;
 	
 	@Override
 	public String getDriver() {
-		return driver;
+		return "org.hsqldb.jdbcDriver";
 	}
 
 	@Override
 	public String getConnection() {
-		return connection;
+		return "jdbc:hsqldb:hsql://localhost/" + dbName;
 	}
 
 	@Override
 	public String getUser() {
-		return "sa";//SysConfig.getDbUserAsString();
+		return "sa";
 	}
 
 	@Override
 	public String getPassword() {
-		return "";//SysConfig.getDbPasswordAsString();
+		return "";
 	}
 
 	@Override
 	public void setUpServer() {
 		try	{
-			server = new Server();
-			
-			server.setLogWriter(null);
-			server.setSilent(true);
-			
-			server.setDatabaseName(0, dbName);
-			server.setDatabasePath(0, "file:" + dbName + "db");
-			
-			SysLog.logInfo("Starting hsql server...");
-			
-			server.start();
-			
-			SysLog.logInfo("Hsql server started.");
+			startServer();
 		} catch (Exception ex){
 			SysLog.logThrowable(ex);
 		}		
 	}
 
+	protected void startServer()	{
+		server = new Server();
+		
+		server.setLogWriter(null);
+		server.setSilent(true);
+		
+		server.setDatabaseName(0, dbName);
+		server.setDatabasePath(0, "file:" + dbName + "db");
+		
+		SysLog.logInfo("Starting hsql server...");
+		
+		server.start();
+		
+		SysLog.logInfo("Hsql server started.");
+	}
+	
 	@Override
 	public void tearDownServer() {
 		SysLog.logInfo("Stopping hsql server.");
--- test/org/wahlzeit/testsetup/DataBaseSetup.java
+++ test/org/wahlzeit/testsetup/DataBaseSetup.java
@@ -1,7 +1,12 @@
 package org.wahlzeit.testsetup;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import org.wahlzeit.services.DatabaseConnection;
+import org.wahlzeit.services.SysLog;
 import org.wahlzeit.services.dbconfig.HyperSQLConfig;
+import org.wahlzeit.tools.RunScript;
+
 import junit.extensions.TestSetup;
 import junit.framework.Test;
 
@@ -15,8 +20,34 @@ public class DataBaseSetup extends TestSetup	{
 		super.setUp();
 		
 		DatabaseConnection.initialize(new HyperSQLConfig());
+
+		if (noData())	{
+			fillDB();
+		}
 	}
 
+	protected boolean noData()	{
+		try	{
+			Connection conn = DatabaseConnection.openRdbmsConnection();
+			conn.createStatement().execute("SELECT count(*) FROM users");
+		} catch (SQLException ex)	{
+			return true;
+		}
+		
+		return false;
+	}
+	
+	protected void fillDB()	{
+		SysLog.logInfo("Filling database");
+		
+		//TODO: hsql does not like the script
+		try	{
+			new RunScript().run(new String[]{ "-S" });
+		} catch (Exception ex)	{
+			
+		}
+	}
+	
 	@Override
 	protected void tearDown() throws Exception {
 		super.tearDown();
