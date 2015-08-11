5b52809efb6c41f881e14dce0e06eff3e62afd75
Lukas Hahmann <tfr.didi@gmail.com>
2015-08-11 02:03:37 +0200

    Outsourced the loading of an image to an own method and commented it.

--- src/main/java/org/wahlzeit/servlets/StaticDataServlet.java
+++ src/main/java/org/wahlzeit/servlets/StaticDataServlet.java
@@ -17,8 +17,8 @@ import java.util.logging.Logger;
 /**
  * Servlet that returns static data like the Photos to the user.
  * <p/>
- * As there are several links for each Photo, this can not the handled via the MainServlet, which has a unique link
- * for each Handler. Instead web.xml redirects all static requests to this Servlet.
+ * As there are several links for each Photo, this can not the handled via the MainServlet, which has a unique link for
+ * each Handler. Instead web.xml redirects all static requests to this Servlet.
  * <p/>
  * Created by Lukas Hahmann on 29.04.15.
  */
@@ -40,19 +40,7 @@ public class StaticDataServlet extends AbstractServlet {
                     addParameter("size", size).toString());
 
             if ("image".equals(type)) {
-                Photo photo = PhotoManager.getInstance().getPhoto(photoId);
-                Image image = null;
-                if(photo != null) {
-                    PhotoSize photoSize = PhotoSize.getFromInt(size);
-                    image = photo.getImage(photoSize);
-                }
-                // if not in cache load from Google Cloud Storage
-                if (image == null) {
-                    Serializable rawImage = ImageStorage.getInstance().readImage(photoId, size);
-                    if (rawImage != null && rawImage instanceof Image) {
-                        image = (Image) rawImage;
-                    }
-                }
+                Image image = getImage(photoId, size);
                 if (image != null) {
                     response.getOutputStream().write(image.getImageData());
                     response.getOutputStream().flush();
@@ -70,6 +58,34 @@ public class StaticDataServlet extends AbstractServlet {
         } catch (Exception e) {
             log.severe(LogBuilder.createSystemMessage().addException("Problem when loading image", e).toString());
         }
+    }
+
+    /**
+     * @methodtype command
+     * <p/>
+     * Loads image either from the <@link>PhotoManager</@link> or from the <@link>ImageStorage</@link>. If image does
+     * not exist, null is returned.
+     */
+    private Image getImage(String photoId, int size) {
+        Image image = null;
+        Photo photo = PhotoManager.getInstance().getPhoto(photoId);
+        if (photo != null) {
+            PhotoSize photoSize = PhotoSize.getFromInt(size);
+            image = photo.getImage(photoSize);
+        }
+        // if not in cache load from Google Cloud Storage
+        if (image == null) {
+            Serializable rawImage = null;
+            try {
+                rawImage = ImageStorage.getInstance().readImage(photoId, size);
+            } catch (IOException e) {
+                log.warning(LogBuilder.createSystemMessage().addException("Problem when reading image.", e).toString());
+            }
+            if (rawImage != null && rawImage instanceof Image) {
+                image = (Image) rawImage;
+            }
+        }
+        return image;
     }
 }
 
