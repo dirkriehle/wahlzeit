8564b0873aa1ed1cfd501b7ee06e8f96dee5ebb9
mai <mai@mai-Lenovo-Z50-70>
2017-11-03 11:44:33 +0100

    1- Add test suit for the existing test cases of the email service
    2- Add test suit to include the email test suit

---
+++ src/test/java/RootTestSuit.java
@@ -0,0 +1,16 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import org.wahlzeit.services.mailing.AllTestsForEmailService;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({AllTestsForEmailService.class})
+
+
+public class RootTestSuit {
+	
+
+}
+
+
