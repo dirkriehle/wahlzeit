a6b8ad3fef2eaafb173cc02680ec9a467005ef8e
knusperkrone <aron-metzig@web.de>
2021-05-10 21:28:14 +0200

    Removed unnecessary throws statements

--- src/main/java/org/wahlzeit_revisited/api/filter/CorsFilter.java
+++ src/main/java/org/wahlzeit_revisited/api/filter/CorsFilter.java
@@ -36,8 +36,7 @@ import java.io.IOException;
 public class CorsFilter implements ContainerResponseFilter {
 
     @Override
-    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
-            throws IOException {
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
         responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
         responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
--- src/main/java/org/wahlzeit_revisited/database/DatabaseConnection.java
+++ src/main/java/org/wahlzeit_revisited/database/DatabaseConnection.java
@@ -172,7 +172,7 @@ public class DatabaseConnection {
     /**
      *
      */
-    public Connection getRdbmsConnection() throws SQLException {
+    public Connection getRdbmsConnection() {
         return rdbmsConnection;
     }
 
