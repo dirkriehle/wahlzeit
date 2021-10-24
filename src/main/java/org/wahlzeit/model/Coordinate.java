7525f175a3d47d5ba0ea95c033f03aa6990e0ecd
Nina Schoppe <nina_schoppe@outlook.de>
2021-10-24 18:50:52 +0200

    add constructor and getters

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -8,4 +8,38 @@ public class Coordinate {
     private double x;
     private double y;
     private double z;
+
+    /**
+     * 
+     * @methodtype constructor
+     */
+    public Coordinate(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    /**
+	 * 
+	 * @methodtype get
+	 */
+    public double getX() {
+        return x;
+    }
+
+    /**
+	 * 
+	 * @methodtype get
+	 */
+    public double getY() {
+        return y;
+    }
+
+    /**
+	 * 
+	 * @methodtype get
+	 */
+    public double getZ() {
+        return z;
+    }
 }
