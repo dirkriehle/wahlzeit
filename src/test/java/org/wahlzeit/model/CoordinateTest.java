19cc95413ebfafe790a18ac6e0eb50400059085d
Nina Schoppe <nina_schoppe@outlook.de>
2021-11-07 16:37:49 +0100

    refactor: imports

--- src/test/java/org/wahlzeit/model/CoordinateTest.java
+++ src/test/java/org/wahlzeit/model/CoordinateTest.java
@@ -1,10 +1,10 @@
 package org.wahlzeit.model;
 
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
 
 public class CoordinateTest {
 
